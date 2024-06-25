8080ab99b2b01a945e76735e8982e3b8c530d919
ば <baisoni.baisony@gmail.com>
2024-06-26 02:57:36 +0900

    Update social_text_span_builder.dart

--- lib/util/social_text_span_builder.dart
+++ lib/util/social_text_span_builder.dart
@@ -65,6 +65,7 @@ class SocialTextSpanBuilder {
     int cursorPosition = 0;
     for (int i = 0; i < orderedMatches.length; i++) {
       var match = orderedMatches[i];
+      print("start: ${match.start}, end: ${match.end}");
       print("Match: ${text.substring(match.start, match.end)}");
       print("Match: ${match.input.substring(match.start, match.end)}");
       var subString = text.substring(match.start, match.end);

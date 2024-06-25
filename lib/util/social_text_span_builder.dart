e6ad186f0f13546e921928b5ebb9d17d75c652db
ば <baisoni.baisony@gmail.com>
2024-06-26 03:19:08 +0900

    Update social_text_span_builder.dart

--- lib/util/social_text_span_builder.dart
+++ lib/util/social_text_span_builder.dart
@@ -3,62 +3,53 @@ import 'package:flutter/material.dart';
 import 'package:flutter_social_textfield/model/detected_type_enum.dart';
 import 'package:flutter_social_textfield/model/social_content_detection_model.dart';
 
-///Builds [TextSpan] with the provided regular expression, stles and text.
-/// [defaultTextStyle] Optional default textstyle. used for detection types that has not been initialied
-/// [detectionTextStyles] required, used for setting up text styles for types found in [DetectedType] enum
-/// [regularExpressions] required, used for detecting [DetectedType] content. default regular expressions can be found in the plugin
-/// [onTapDetection] optional. When set, it assings [TapGestureRecognizer] to formatted content. It returns [SocialContentDetection] as response
-/// [ignoredTextStyle] optional. When set, content matched with "ignoreCases" of build function will return this text style, returns default text style if null.
-class SocialTextSpanBuilder{
-
+class SocialTextSpanBuilder {
   final Function(SocialContentDetection detection)? onTapDetection;
   final TextStyle? defaultTextStyle;
   final TextStyle? ignoredTextStyle;
   final Map<DetectedType, TextStyle> detectionTextStyles;
-
   final Map<DetectedType, RegExp> regularExpressions;
-
   Map<DetectedType, List<RegExpMatch>?> allMatches = Map();
 
-  SocialTextSpanBuilder({required this.regularExpressions,required this.defaultTextStyle,this.detectionTextStyles = const {},this.onTapDetection, this.ignoredTextStyle});
+  SocialTextSpanBuilder({
+    required this.regularExpressions,
+    required this.defaultTextStyle,
+    this.detectionTextStyles = const {},
+    this.onTapDetection,
+    this.ignoredTextStyle,
+  });
 
-  ///Gets Text Style for [start,end] range.
-  ///return TextStyle() style if nothing found.
-  MatchSearchResult getTextStyleForRange(int start, int end, {List<String>? ignoreCases, List<String>? includeOnlyCases}){
-
+  MatchSearchResult getTextStyleForRange(int start, int end,
+      {List<String>? ignoreCases, List<String>? includeOnlyCases}) {
     TextStyle? textStyle;
     DetectedType detectedType = DetectedType.plain_text;
     String text = "";
     allMatches.keys.forEach((type) {
-      var index = allMatches[type]!.indexWhere((match) => match.start == start && match.end == end);
+      var index =
+      allMatches[type]!.indexWhere((match) => match.start == start && match.end == end);
 
-      if(index != -1){
-        text = allMatches[type]![index].input.substring(start,end);
+      if (index != -1) {
+        text = allMatches[type]![index].input.substring(start, end);
         var isIgnored = false;
-        if(includeOnlyCases?.isNotEmpty ?? false){
-          isIgnored = (includeOnlyCases?.indexWhere((t)=>t == text.trim()) ?? -1) == -1;
-        }else{
-          isIgnored = (ignoreCases?.indexWhere((t)=>t == text.trim()) ?? -1) >= 0;
+        if (includeOnlyCases?.isNotEmpty ?? false) {
+          isIgnored = (includeOnlyCases?.indexWhere((t) => t == text.trim()) ?? -1) == -1;
+        } else {
+          isIgnored = (ignoreCases?.indexWhere((t) => t == text.trim()) ?? -1) >= 0;
         }
-        if(isIgnored){
+        if (isIgnored) {
           textStyle = ignoredTextStyle;
           detectedType = DetectedType.plain_text;
-        }else{
+        } else {
           textStyle = detectionTextStyles[type];
           detectedType = type;
         }
         return;
       }
     });
-    return MatchSearchResult(textStyle ?? defaultTextStyle ?? TextStyle(), detectedType,text);
+    return MatchSearchResult(textStyle ?? defaultTextStyle ?? TextStyle(), detectedType, text);
   }
 
-  ///returns TextSpan containing all formatted content.
-  ///[text] Text Content
-  ///[ignoreCases] optional, when set, string values written in ignoreCases will be treated as Plain Text
-  ///[includeOnlyCases] optional, when set, only values found in this array will be detected, other values be treated as Plain Text
   TextSpan build(String text, {List<String>? ignoreCases, List<String>? includeOnlyCases}) {
-    print(text);
     regularExpressions.keys.forEach((type) {
       allMatches[type] = regularExpressions[type]!.allMatches(text).toList();
     });
@@ -74,63 +65,58 @@ class SocialTextSpanBuilder{
     int cursorPosition = 0;
     for (int i = 0; i < orderedMatches.length; i++) {
       var match = orderedMatches[i];
-      if (cursorPosition < match.start) {
-        spans.add(TextSpan(
-          text: text.substring(cursorPosition, match.start),
-          style: defaultTextStyle,
-        ));
-      }
-      var matchedText = text.substring(match.start, match.end);
-      var search = getTextStyleForRange(match.start, match.end, ignoreCases: ignoreCases, includeOnlyCases: includeOnlyCases);
-      TapGestureRecognizer? tapRecognizer;
+      print("Match: ${text.substring(match.start, match.end)}");
+      print("Match: ${match.input.substring(match.start, match.end)}");
+
+      var firstSearch = getTextStyleForRange(cursorPosition, match.start,
+          ignoreCases: ignoreCases, includeOnlyCases: includeOnlyCases);
+      spans.add(TextSpan(
+        text: text.substring(cursorPosition, match.start),
+        style: firstSearch.textStyle,
+      ));
+
+      var secondSearch = getTextStyleForRange(match.start, match.end,
+          ignoreCases: ignoreCases, includeOnlyCases: includeOnlyCases);
+      TapGestureRecognizer? tapRecognizer2;
       if (onTapDetection != null) {
-        tapRecognizer = TapGestureRecognizer()
+        tapRecognizer2 = TapGestureRecognizer()
           ..onTap = () {
             onTapDetection!(SocialContentDetection(
-              search.type,
+              secondSearch.type,
               TextRange(start: match.start, end: match.end),
-              matchedText,
+              secondSearch.text,
             ));
           };
       }
       spans.add(TextSpan(
-        text: matchedText,
-        style: search.textStyle,
-        recognizer: tapRecognizer,
+        text: text.substring(match.start, match.end),
+        style: secondSearch.textStyle,
+        recognizer: tapRecognizer2,
       ));
       cursorPosition = match.end;
     }
     if (cursorPosition < text.length) {
       spans.add(TextSpan(
         text: text.substring(cursorPosition),
-        style: defaultTextStyle,
+        style: getTextStyleForRange(cursorPosition, text.length).textStyle,
       ));
     }
-    print("spans: ${spans}");
     return TextSpan(children: spans);
   }
 
-  ///Wraps text with style inside the root.
-  ///[root] optional, return TextSpan(text, style:style) if null
-  ///[text] main content text
-  ///[style] TextStyle
-  ///[tapRecognizer] optional, tap action for detected content
-  TextSpan getTextSpan(TextSpan? root, String text, TextStyle style,{TapGestureRecognizer? tapRecognizer}){
-    if(root == null){
-      return TextSpan(text: text,style: style,recognizer: tapRecognizer);
-    }else{
-      return TextSpan(children: [root, TextSpan(text: text, style: style,recognizer: tapRecognizer)]);
+  TextSpan getTextSpan(TextSpan? root, String text, TextStyle style,
+      {TapGestureRecognizer? tapRecognizer}) {
+    if (root == null) {
+      return TextSpan(text: text, style: style, recognizer: tapRecognizer);
+    } else {
+      return TextSpan(children: [root, TextSpan(text: text, style: style, recognizer: tapRecognizer)]);
     }
   }
 }
 
-///Used internally by SocialTextSpanBuilder
-///[textStyle] matched textstyle, return default if no matches fonud
-///[type] detected type. returns [DetectedType.plain_text] by default.
-///[text] returns text within range. returns empty string if no matches found.
-class MatchSearchResult{
+class MatchSearchResult {
   final TextStyle textStyle;
   final DetectedType type;
   final String text;
-  MatchSearchResult(this.textStyle, this.type,this.text);
+  MatchSearchResult(this.textStyle, this.type, this.text);
 }
